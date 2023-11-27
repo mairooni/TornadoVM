fff6824086b0062a4ebfd3829012fe3ace7d4238
Juan Fumero <juan.fumero@manchester.ac.uk>
2023-11-27 16:14:07 +0100

    SPIR-V intrinsic replacements fixed

--- tornado-drivers/spirv/src/main/java/uk/ac/manchester/tornado/drivers/spirv/graal/phases/TornadoSPIRVIntrinsicsReplacements.java
+++ tornado-drivers/spirv/src/main/java/uk/ac/manchester/tornado/drivers/spirv/graal/phases/TornadoSPIRVIntrinsicsReplacements.java
@@ -13,7 +13,7 @@
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * version 2 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
  *
@@ -178,12 +178,16 @@ public class TornadoSPIRVIntrinsicsReplacements extends BasePhase<TornadoHighTie
     private JavaKind getJavaKindFromConstantNode(ConstantNode signatureNode) {
         switch (signatureNode.getValue().toValueString()) {
             case "Class:int":
+            case "Class:uk.ac.manchester.tornado.api.types.arrays.IntArray":
                 return JavaKind.Int;
             case "Class:long":
+            case "Class:uk.ac.manchester.tornado.api.types.arrays.LongArray":
                 return JavaKind.Long;
             case "Class:float":
+            case "Class:uk.ac.manchester.tornado.api.types.arrays.FloatArray":
                 return JavaKind.Float;
             case "Class:double":
+            case "Class:uk.ac.manchester.tornado.api.types.arrays.DoubleArray":
                 return JavaKind.Double;
             default:
                 unimplemented("Other types not supported yet: " + signatureNode.getValue().toValueString());
